86744c3bf41628382a31aea1f369519e32de3655
Ryan Milligan <ryanmilligan88@gmail.com>
2026-03-14 16:27:43 -0700

    use aflplusplus image

--- build-jq.sh
+++ build-jq.sh
@@ -5,6 +5,6 @@ set -e
 git submodule update --init 
 autoreconf -fi 
 export AFL_USE_ASAN=1
-CC=afl-clang-fast CXX=afl-clang-fast++ ./configure --with-oniguruma=builtin --enable-all-static 
+CC=afl-clang-fast CXX=afl-clang-fast++ ./configure --with-oniguruma=builtin --enable-all-static --disable-shared
 make clean
 make -j8
--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker image prune -f
-docker compose run --build --rm -it -v $(pwd):/workspace ubuntu-fuzz bash
+# docker image prune -f
+# docker compose run --build --rm -it -v $(pwd):/workspace ubuntu-fuzz bash
+
+docker compose run -it -v $(pwd):/workspace afl
