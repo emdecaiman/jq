2b5cf81b5d3541195f26801014443df09c6d1e46
Ryan Milligan <ryanmilligan88@gmail.com>
2026-03-15 11:08:20 -0700

    add build scripts for other sanitizers

--- build-msan.sh
+++ build-msan.sh
@@ -4,7 +4,7 @@ set -e
 
 git submodule update --init 
 autoreconf -fi 
-export AFL_HARDEN=1
+export AFL_USE_MSAN=1
 ENABLE_ALL_STATIC=1 CC=afl-clang-fast CXX=afl-clang-fast++ ./configure --with-oniguruma=builtin --enable-all-static --disable-shared
 make clean
 make -j8
