51f2cf72c5635666cfbb8fe4e35bc64064b3de82
Ryan Milligan <ryanmilligan88@gmail.com>
2026-03-15 10:55:21 -0700

    split build scripts

---
+++ build-asan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+git submodule update --init 
+autoreconf -fi 
+export AFL_USE_ASAN=1
+ENABLE_ASAN=1 ENABLE_ALL_STATIC=1 CC=afl-clang-fast CXX=afl-clang-fast++ ./configure --with-oniguruma=builtin --enable-all-static --disable-shared
+make clean
+make -j8
--- fuzz-json.sh
+++ fuzz-json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-AFL_IGNORE_SEED_PROBLEMS=1  afl-fuzz -i fuzz/input/cli_json -o fuzz/output/cli_json -- ./jq '.' @@
+AFL_SKIP_CPUFREQ=1 AFL_IGNORE_SEED_PROBLEMS=1  afl-fuzz -i fuzz/input/cli_json -o fuzz/output/cli_json  -t 10000  -- ./jq '.' @@
 
